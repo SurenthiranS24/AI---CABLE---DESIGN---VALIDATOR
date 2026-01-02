130124cd6330edd7608e2572eb778dfb1ee050cc
SurenthiranS24 <surenthiran333me@gmail.com>
2026-01-02 23:59:30 +0530

    Add files via upload

---
+++ design-validator-backend/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DesignValidationModule } from './design-validation/design-validation.module';
+
+@Module({
+  imports: [DesignValidationModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
